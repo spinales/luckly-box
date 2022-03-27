4ccb3c67f32a5d49630a7bf88d07b5c65955f7b0
spinales <saul021002@gmail.com>
2022-03-27 10:40:26 -0400

    deploy

--- assets/AboutView.0feb6abd.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.99845e6f.js";import{o as t,c as o,a as _}from"./vendor.7fe84ada.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
